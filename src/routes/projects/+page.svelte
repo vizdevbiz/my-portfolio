2d79c713037ee297678012ceda61fc342809352e
VizDev Biz <vizdevbiz@gmail.com>
2025-12-26 16:43:54 +0100

    Template projects from a data file

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,162 +1,18 @@
+<script>
+  import projects from '$lib/projects.json';
+</script>
+
+
 <svelte:head>
 	<title>Projects</title>
 </svelte:head>
-<h1>Projects</h1>
+<h1>{projects.length} Projects</h1>
     <div class="projects">
-      <article>
-        <h2>Lorem ipsum dolor sit.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
-          cum inventore obcaecati quidem sint itaque perferendis recusandae qui?
-          Ipsum autem nobis aspernatur nisi! Dolores provident, commodi dicta
-          nihil officia eius!
-        </p>
-      </article>
-      <article>
-        <h2>Atque error iure sapiente.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Dolorum architecto consequatur quo temporibus commodi qui sint hic,
-          repellendus veritatis. Ea facere enim adipisci rerum velit! Illum,
-          repellendus ex, in ad optio molestiae voluptatum praesentium tenetur
-          quibusdam quis unde!
-        </p>
-      </article>
-      <article>
-        <h2>Autem aperiam exercitationem optio?</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Voluptatem enim eveniet nesciunt autem, et inventore velit minus esse,
-          similique quas perferendis deserunt. Maxime iure saepe, est,
-          repellendus minima porro animi cumque aspernatur amet in, sed soluta
-          tempore hic?
-        </p>
-      </article>
-      <article>
-        <h2>Molestiae eligendi modi excepturi.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Veritatis adipisci excepturi voluptate. Assumenda quia blanditiis
-          dolorem enim provident? Odio, harum, ratione sequi accusantium dolore
-          necessitatibus sint labore voluptate numquam aut at, asperiores
-          tenetur adipisci nostrum corporis officia ut!
-        </p>
-      </article>
-      <article>
-        <h2>Aliquam nemo cumque repellendus.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Rerum sint tempore ipsa, placeat ullam eaque quaerat aperiam eius
-          dolorum. Placeat voluptate consequuntur a facilis quasi fugit
-          dignissimos! Earum corrupti cum aperiam ex temporibus totam fugit vero
-          voluptatum harum!
-        </p>
-      </article>
-      <article>
-        <h2>Quia aspernatur dignissimos quo.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Dolores iste, nulla placeat dignissimos atque ea excepturi? Nulla,
-          nostrum! Dolorum hic porro exercitationem impedit ipsa fugit quo
-          accusamus nisi vel at quam quaerat, rerum dicta facere mollitia? Id,
-          hic?
-        </p>
-      </article>
-      <article>
-        <h2>Animi, ratione eveniet. Magni.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Qui corrupti eaque et doloremque aliquam quae non iure, cupiditate
-          delectus vel exercitationem nam officiis magni nihil architecto
-          facilis amet, magnam provident adipisci velit, id quis harum. Ipsa,
-          pariatur optio?
-        </p>
-      </article>
-      <article>
-        <h2>Quidem odit doloribus obcaecati?</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Neque maxime, impedit excepturi illo vel minima optio error molestiae
-          fugit, quod sunt, nostrum quaerat. Consequuntur numquam dolorem,
-          obcaecati eligendi optio possimus voluptatem cumque molestias facere
-          dolores in aliquid blanditiis?
-        </p>
-      </article>
-      <article>
-        <h2>Minima eos numquam iste.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Quod dolores, aliquid laudantium rerum totam, qui modi minus ipsa
-          facilis non ducimus maxime id recusandae optio quaerat atque iure
-          natus magni ea eligendi consequuntur animi. Magnam, dignissimos
-          laboriosam. Aperiam.
-        </p>
-      </article>
-      <article>
-        <h2>Officia, impedit rerum. Alias!</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Enim, aspernatur accusantium voluptate nemo quod adipisci consequatur
-          fugit molestiae ipsam quam tempora libero beatae asperiores alias non
-          iusto? Ad quidem corrupti debitis repellendus dolore reprehenderit
-          tenetur similique nam molestiae.
-        </p>
-      </article>
-      <article>
-        <h2>Illo assumenda dolorum quas.</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Sed architecto cupiditate nam quasi quo dolorum nihil, quis,
-          temporibus, vero nulla recusandae nisi? Nisi ducimus optio odit
-          asperiores dolorem veritatis ut? Corporis, delectus enim nihil hic
-          repellendus deleniti officia.
-        </p>
-      </article>
-      <article>
-        <h2>Rerum, porro saepe? Corporis?</h2>
-        <img
-          src="https:/dig.cmu.edu/datavis-fall-2025/labs/2/images/empty.svg"
-          alt=""
-        />
-        <p>
-          Eius placeat a, at accusantium iusto, maxime rem non voluptates
-          tempora sint consectetur? Similique hic expedita fugit optio ipsa
-          ducimus nihil totam cumque sint? Incidunt cupiditate quasi maiores
-          labore nihil?
-        </p>
-      </article>
+      {#each projects as p}
+        <article>
+          <h2>{p.title}</h2>
+          <img src={p.image} alt="" />
+          <p>{p.description}</p>
+        </article>
+      {/each}
     </div>
