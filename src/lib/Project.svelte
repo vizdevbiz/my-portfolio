6cdd2977bfb0b0d8635a9bf46baf5f4daae734c9
VizDev Biz <vizdevbiz@gmail.com>
2025-12-26 17:14:51 +0100

    Use project component

---
+++ src/lib/Project.svelte
@@ -0,0 +1,21 @@
+<script>
+  let { data = {}, hLevel = 2 } = $props();
+</script>
+
+<article>
+  <svelte:element this={'h' + hLevel}>{data.title}</svelte:element>
+  <img src={data.image} alt="" />
+  <p>{data.description}</p>
+</article>
+
+<style>
+  article {
+    display: grid;
+    grid-template-rows: subgrid;
+    grid-row: span 3;
+  }
+
+  img {
+    max-width: 90%;
+  }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,18 +1,14 @@
 <script>
   import projects from '$lib/projects.json';
+  import Project from '$lib/Project.svelte';
 </script>
 
-
 <svelte:head>
-	<title>Projects</title>
+  <title>Projects</title>
 </svelte:head>
 <h1>{projects.length} Projects</h1>
-    <div class="projects">
-      {#each projects as p}
-        <article>
-          <h2>{p.title}</h2>
-          <img src={p.image} alt="" />
-          <p>{p.description}</p>
-        </article>
-      {/each}
-    </div>
+<div class="projects">
+  {#each projects as p}
+    <Project data={p} />
+  {/each}
+</div>
