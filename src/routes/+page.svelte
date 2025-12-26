b3223b67f8505443ccd2971b3fb41ccb7e290412
VizDev Biz <vizdevbiz@gmail.com>
2025-12-26 17:15:25 +0100

    Add latest projects to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
+<script>
+  import projects from '$lib/projects.json';
+  import Project from '$lib/Project.svelte';
+</script>
+
 <h1>Hi! It's VizDev Biz.</h1>
-    <p>
-      This is my test portfolio site. This is my test portfolio site. This is my
-      test portfolio site. This is my test portfolio site. This is my test
-      portfolio site. This is my test portfolio site.
-    </p>
-    <img src="images/golden-retriever.png" alt="A Golden Retriever puppy" />
+<p>
+  This is my test portfolio site. This is my test portfolio site. This is my
+  test portfolio site. This is my test portfolio site. This is my test portfolio
+  site. This is my test portfolio site.
+</p>
+<img src="images/golden-retriever.png" alt="A Golden Retriever puppy" />
+
+<h2>LatestProjects</h2>
+<div class="projects highlights">
+  {#each projects.slice(0, 3) as p}
+    <Project data={p} hLevel={3} />
+  {/each}
+</div>
